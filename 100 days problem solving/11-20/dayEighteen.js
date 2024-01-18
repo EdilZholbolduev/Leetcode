97e38969eda53189f18760d60dcdb9d8e89a77c4
Edil Zholbolduev <tashtan841@gmail.com>
2024-01-18 03:47:43 +0100

    Climbing Stairs

---
+++ 100 days problem solving/11-20/dayEighteen.js
@@ -0,0 +1,14 @@
+//Climbing Stairs 
+
+var climbStairs = function(n) {
+   let a = 0;
+   let b = 1;
+
+   for (let i = 1; i <= n; i++) {
+       let ways = a + b;
+       a = b;
+       b = ways;
+   }
+
+   return b;
+}
